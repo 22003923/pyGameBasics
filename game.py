d3c44630ac05c5b6f6d9dd0a61fab0a7d2e8ab05
melisa <irem.akel@ug.bilkent.edu.tr>
2023-04-09 12:55:45 +0300

    move

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
 import pygame
+
 pygame.init()
-win = pygame.display.set_mode((500, 500))
+WIDTH, HEIGHT = 900, 500  # screen size
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():#  list of events
+            if even.type == pygame.QUIT:
+                run = false;
+
+    pygame.quit()
